ce7af2b5a7e26384f96d2a2df4d77e5127340eb9
Jean Carlos de Oliveira <jeancarlosdeoliveira@outlook.com>
2020-03-04 15:36:34 -0300

    Dark Mode
    Dark Mode

--- js/script.js
+++
@@ -1,7 +0,0 @@
-document
-    .querySelector('header button')
-    .addEventListener("click", () => {
-        document
-        .querySelector('.form')
-        .classList.toggle('hide')
-    }); 
--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,35 @@ document
     .querySelector('header button')
     .addEventListener("click", () => {
         document
-        .querySelector('.form')
-        .classList.toggle('hide')
-    }); 
+            .querySelector('.form')
+            .classList.toggle('hide')
+    });
+
+
+// DARK MODE
+document
+    .querySelector('#switch-shadow')
+    .addEventListener('click', () => {
+        document
+            .querySelector('body')
+            .classList.toggle('dark');
+
+        document
+            .getElementById('main')
+            .classList.toggle('dark');
+        document
+            .getElementById('footer')
+            .classList.toggle('dark');
+        document
+            .getElementById('header')
+            .classList.toggle('dark');
+        document
+            .getElementById('switch__container')
+            .classList.toggle('dark');
+
+        if (document.getElementById('header').classList.contains('dark')) {
+            document.getElementById("logo").src = "./img/logo_dark.png";
+        } else {
+            document.getElementById("logo").src = "./img/logo.png";
+        }
+    });
